b32ff1ce427324187b372b263d2afb88fdedb5eb
ChristianNZ6 <christianjorge2009@gmail.com>
2018-10-01 12:01:26 -0300

    tst

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChildren, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -10,13 +10,15 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor:number = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput:ElementRef;
+
   incrementa(){
-this.valor++;
-this.mudouValor.emit({ novoValor:this.valor });
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({ novoValor:this.valor });
   }
   decrementa(){
 
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
